d31a78e84d67e269689af14d4e4dc04d5c634d5a
joindevops <info@joindevops.com>
2024-04-19 07:42:07 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -36,6 +36,12 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-useradd expense
-VALIDATE $? "Creating expense user"
+id expense -y &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd expense -y &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
 
