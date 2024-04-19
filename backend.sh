71334e45a7f317b1d316f21931187244fd276f9a
joindevops <info@joindevops.com>
2024-04-19 07:56:44 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -54,6 +54,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading backend code"
 
 cd /app
+rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
 VALIDATE $? "Extracted backend code"
 
